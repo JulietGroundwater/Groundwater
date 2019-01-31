a89d2d810b8b92d694a21ea2fc4bf5c460c0cd8f
Chris Baish <chris.baish@gmail.com>
2019-01-31 15:46:23 +0000

    Added a hello world main class for testing the computation engine with.

---
+++ computationEngine/src/main/java/uk/ac/cam/cl/juliet/computationenginetest/ComputationEngineTest.java
@@ -0,0 +1,12 @@
+package uk.ac.cam.cl.juliet.computationenginetest;
+
+import uk.ac.cam.cl.juliet.computationengine.ComputationEngine;
+
+public class ComputationEngineTest
+{
+    public static void main(String [ ] args)
+    {
+        ComputationEngine engine = new ComputationEngine();
+        System.out.println("Hello World");
+    }
+}
