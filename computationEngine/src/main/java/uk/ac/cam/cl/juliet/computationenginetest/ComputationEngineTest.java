9f576b53f7d95fb65422c3f2a2abecb56d0de321
Chris Baish <chris.baish@gmail.com>
2019-01-31 16:00:07 +0000

    Added Javadoc comments for ComputationEngineTest

--- computationEngine/src/main/java/uk/ac/cam/cl/juliet/computationenginetest/ComputationEngineTest.java
+++ computationEngine/src/main/java/uk/ac/cam/cl/juliet/computationenginetest/ComputationEngineTest.java
@@ -2,10 +2,17 @@ package uk.ac.cam.cl.juliet.computationenginetest;
 
 import uk.ac.cam.cl.juliet.computationengine.ComputationEngine;
 
-public class ComputationEngineTest
-{
-    public static void main(String [ ] args)
-    {
+/**
+ * Class used to allow the running of a {@code ComputationEngine} without requiring the android app.
+ */
+public class ComputationEngineTest {
+
+    /**
+     * The main function for the app.
+     *
+     * @param args The command line arguments.
+     */
+    public static void main(String[] args) {
         ComputationEngine engine = new ComputationEngine();
         System.out.println("Hello World");
     }
