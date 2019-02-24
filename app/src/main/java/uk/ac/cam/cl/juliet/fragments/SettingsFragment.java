92256a5acc29f1558ffd7e76f5fc1593b3267577
Ben Cole <bencole12345@gmail.com>
2019-02-24 22:07:16 +0000

    Implement GPS location dialog

--- app/src/main/java/uk/ac/cam/cl/juliet/fragments/SettingsFragment.java
+++ app/src/main/java/uk/ac/cam/cl/juliet/fragments/SettingsFragment.java
@@ -1,8 +1,12 @@
 package uk.ac.cam.cl.juliet.fragments;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
@@ -10,18 +14,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 import uk.ac.cam.cl.juliet.R;
-import uk.ac.cam.cl.juliet.computationengine.Config;
-import uk.ac.cam.cl.juliet.computationengine.InvalidConfigException;
 import uk.ac.cam.cl.juliet.data.AttenuatorSettings;
 import uk.ac.cam.cl.juliet.dialogs.AttenuatorsDialog;
 
@@ -33,37 +35,33 @@ import uk.ac.cam.cl.juliet.dialogs.AttenuatorsDialog;
 public class SettingsFragment extends Fragment
         implements AttenuatorsDialog.OnAttenuatorsSelectedListener, Button.OnClickListener {
 
-    private static double ATTENUATION_MIN = 2;
-    private static double ATTENUATION_MAX = 10;
-
-    private Switch usePhoneGPSSwitch;
     private TextView connectionStatusText;
     private ImageView connectionStatusIcon;
     private TextView selectedDateOutput;
-    private Button setDateButton;
     private TextView selectedTimeOutput;
+    private TextView latitudeOutput;
+    private TextView longitudeOutput;
+    private Button setDateButton;
     private Button setTimeButton;
+    private Button setGPSButton;
     private Button configureAttenuatorsButton;
+    private Button sendToDeviceButton;
 
+    private int minute;
+    private int hourOfDay;
+    private int day;
+    private int month;
+    private int year;
+    private double latitude;
+    private double longitude;
     private List<Integer> attenuators;
     private List<Integer> gains;
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-
-        usePhoneGPSSwitch = view.findViewById(R.id.useThisPhoneGPSSwitch);
-        usePhoneGPSSwitch.setOnCheckedChangeListener(
-                new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if (!isChecked) {
-                            showSetLocationDialog();
-                        }
-                    }
-                });
 
         connectionStatusText = view.findViewById(R.id.connectionStatusText);
         connectionStatusIcon = view.findViewById(R.id.connectionStatusImageView);
@@ -71,17 +69,22 @@ public class SettingsFragment extends Fragment
         // Find the text views
         selectedDateOutput = view.findViewById(R.id.selectedDateText);
         selectedTimeOutput = view.findViewById(R.id.selectedTimeText);
+        latitudeOutput = view.findViewById(R.id.latitudeText);
+        longitudeOutput = view.findViewById(R.id.longitudeText);
 
         // Find the buttons and set this class as the click listener
         setDateButton = view.findViewById(R.id.setDateButton);
         setDateButton.setOnClickListener(this);
         setTimeButton = view.findViewById(R.id.setTimeButton);
         setTimeButton.setOnClickListener(this);
+        setGPSButton = view.findViewById(R.id.setGPSButton);
+        setGPSButton.setOnClickListener(this);
         configureAttenuatorsButton = view.findViewById(R.id.configureAttenuatorsButton);
         configureAttenuatorsButton.setOnClickListener(this);
+        sendToDeviceButton = view.findViewById(R.id.sendToDeviceButton);
+        sendToDeviceButton.setOnClickListener(this);
 
-        attenuators = new ArrayList<>();
-        gains = new ArrayList<>();
+        setDefaultValues();
 
         setConnectedStatus(getConnectionStatus());
         return view;
@@ -89,10 +92,11 @@ public class SettingsFragment extends Fragment
 
     @Override
     public void onResume() {
+        Activity activity = getActivity();
+        if (activity == null) return;
         super.onResume();
         try {
-            getActivity()
-                    .getWindow()
+            activity.getWindow()
                     .setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
         } catch (NullPointerException e) {
             e.printStackTrace();
@@ -118,8 +122,26 @@ public class SettingsFragment extends Fragment
         }
     }
 
+    /** Sets the default values for when the screen is first loaded. */
+    private void setDefaultValues() {
+        Calendar calendar = Calendar.getInstance();
+        minute = calendar.get(Calendar.MINUTE);
+        hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+        onNewTimeSet(hourOfDay, minute);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        month = calendar.get(Calendar.MONTH);
+        year = calendar.get(Calendar.YEAR);
+        onNewDateSet(year, month, day);
+        attenuators = new ArrayList<>();
+        attenuators.add(15);
+        gains = new ArrayList<>();
+        gains.add(-14);
+    }
+
+    /** Displays a dialog for setting the time of the radar device. */
     private void showSetTimeDialog() {
-        // TODO: implement
+        Context context = getContext();
+        if (context == null) return;
 
         // Get current time
         Calendar calendar = Calendar.getInstance();
@@ -128,7 +150,7 @@ public class SettingsFragment extends Fragment
 
         TimePickerDialog dialog =
                 new TimePickerDialog(
-                        getContext(),
+                        context,
                         new TimePickerDialog.OnTimeSetListener() {
                             @Override
                             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -142,14 +164,23 @@ public class SettingsFragment extends Fragment
         dialog.show();
     }
 
+    /**
+     * Callback for when the dialog completes and a new time has been chosen.
+     *
+     * @param hour The hourOfDay of the day
+     * @param minute The minute of the hourOfDay
+     */
     private void onNewTimeSet(int hour, int minute) {
-        // TODO: actually save these numbers!
+        this.hourOfDay = hour;
+        this.minute = minute;
         // TODO: handle string formatting properly
         selectedTimeOutput.setText(hour + ":" + minute);
     }
 
+    /** Displays a dialog for setting the date of the radar device. */
     private void showSetDateDialog() {
-        // TODO: implement
+        Context context = getContext();
+        if (context == null) return;
 
         // Get current date
         Calendar calendar = Calendar.getInstance();
@@ -159,7 +190,7 @@ public class SettingsFragment extends Fragment
 
         DatePickerDialog dialog =
                 new DatePickerDialog(
-                        getContext(),
+                        context,
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(
@@ -174,14 +205,68 @@ public class SettingsFragment extends Fragment
         dialog.show();
     }
 
+    /**
+     * Callback for when the dialog completes and a new date has been chosen.
+     *
+     * @param year The year that was selected
+     * @param month The month of the year
+     * @param day The day of the month
+     */
     private void onNewDateSet(int year, int month, int day) {
-        // TODO: actually save these numbers!
         // TODO: handle string formatting properly
+        this.year = year;
+        this.month = month;
+        this.day = day;
         selectedDateOutput.setText(day + "/" + month + "/" + year);
     }
 
     private void showSetLocationDialog() {
         // TODO: implement
+        Context context = getContext();
+        if (context == null) return;
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.dialog_gps_coordinates);
+        final TextView latitudeInput = dialog.findViewById(R.id.latitudeInput);
+        final TextView longitudeInput = dialog.findViewById(R.id.longitudeInput);
+        dialog.findViewById(R.id.setButton)
+                .setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                double latitude =
+                                        Double.parseDouble(latitudeInput.getText().toString());
+                                double longitude =
+                                        Double.parseDouble(longitudeInput.getText().toString());
+                                dialog.cancel();
+                                onNewLocationSet(latitude, longitude);
+                            }
+                        });
+        dialog.findViewById(R.id.cancelButton)
+                .setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.cancel();
+                            }
+                        });
+        dialog.show();
+    }
+
+    /**
+     * Callback for when a new location is selected.
+     *
+     * @param latitude The latitude that was selected
+     * @param longitude The longitude that was selected
+     */
+    private void onNewLocationSet(double latitude, double longitude) {
+        if ((-90 <= latitude) && (latitude <= 90) && (-180 <= longitude) && (longitude <= 180)) {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            latitudeOutput.setText(String.format(Locale.getDefault(), "%f", latitude));
+            longitudeOutput.setText(String.format(Locale.getDefault(), "%f", longitude));
+        } else {
+            Toast.makeText(getContext(), R.string.invalid_gps_coords, Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void showAttenuatorsDialog() {
@@ -196,17 +281,11 @@ public class SettingsFragment extends Fragment
         dialog.show(fragmentManager, "AttenuatorsDialog");
     }
 
-    private Config generateConfig() {
+    /** Handles packaging up the configuration settings and sending them to the device. */
+    private void sendToDevice() {
         // TODO: Implement this
         // TODO: we need a way to build a config file by setting params, then let the Config class
         //       write those params to a file.
-        try {
-            Config config = new Config(null);
-        } catch (InvalidConfigException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
     @Override
@@ -224,8 +303,14 @@ public class SettingsFragment extends Fragment
             case R.id.setTimeButton:
                 showSetTimeDialog();
                 break;
+            case R.id.setGPSButton:
+                showSetLocationDialog();
+                break;
             case R.id.configureAttenuatorsButton:
                 showAttenuatorsDialog();
+                break;
+            case R.id.sendToDeviceButton:
+                sendToDevice();
         }
     }
 }
